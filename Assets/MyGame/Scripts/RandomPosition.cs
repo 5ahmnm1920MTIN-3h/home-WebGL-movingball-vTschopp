a20c8a71da97c565da572670bdf455a6feef64a9
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2020-01-13 20:53:35 +0100

    Add simple script to move Tux randomly

---
+++ Assets/MyGame/Scripts/RandomPosition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    [SerializeField] GameObject tux;
+    [SerializeField] GameObject[] possiblePositions = new GameObject[4];
+
+    public void ChangePosition()
+    {
+        tux.transform.position = possiblePositions[Random.Range(0, possiblePositions.Length)].transform.position;
+    }
+}
